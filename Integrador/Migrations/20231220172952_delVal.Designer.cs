216ff64a3369c79da89eff4a1e462b37a3588adf
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2023-12-20 18:31:52 +0100

    Migracion quitar validaciones

--- Integrador/Migrations/20231220172952_delVal.Designer.cs
+++ Integrador/Migrations/20231220172952_delVal.Designer.cs
@@ -4,6 +4,7 @@ using Integrador.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Integrador.Migrations
 {
     [DbContext(typeof(IntegradorContexto))]
-    partial class IntegradorContextoModelSnapshot : ModelSnapshot
+    [Migration("20231220172952_delVal")]
+    partial class delVal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Integrador/Migrations/20231220172952_delVal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Integrador.Migrations
+{
+    /// <inheritdoc />
+    public partial class delVal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaConfirmacion",
+                table: "Pedido",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaDevolucion",
+                table: "Pedido",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Descuento",
+                table: "DetallePedido",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaConfirmacion",
+                table: "Pedido");
+
+            migrationBuilder.DropColumn(
+                name: "FechaDevolucion",
+                table: "Pedido");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Descuento",
+                table: "DetallePedido",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+        }
+    }
+}
