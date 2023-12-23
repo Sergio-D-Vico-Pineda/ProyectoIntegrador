3384a51d7ca3fa5c0c8a052d749490b548b428a9
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2023-12-23 23:40:20 +0100

    Escaparate WIP

---
+++ Integrador/Controllers/EscaparateController.cs
@@ -0,0 +1,28 @@
+﻿using Integrador.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integrador.Controllers
+{
+    public class EscaparateController : Controller
+    {
+
+        private readonly IntegradorContexto _context;
+
+        public EscaparateController(IntegradorContexto context)
+        {
+            _context = context;
+        }
+
+        // GET /Escaparate/Index
+        public async Task<IActionResult> Index(int? MarcaId)
+        {
+            ViewData["ListaMarcas"] = new SelectList(_context.Marcas, "Id", "Nombre");
+            var productos = _context.Productos
+                .Include(p => p.Modelo)
+                .Where(p => p.Escaparate);
+            return View(await productos.ToListAsync());
+        }
+    }
+}
