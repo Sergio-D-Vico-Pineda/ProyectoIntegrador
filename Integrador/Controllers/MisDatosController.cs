6f3282e6a360aff67728d40626c50bab1ef3bc1e
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2023-12-27 22:32:52 +0100

    Datos de los clientes y los proveedores

---
+++ Integrador/Controllers/MisDatosController.cs
@@ -0,0 +1,65 @@
+﻿using Integrador.Data;
+using Integrador.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integrador.Controllers
+{
+    public class MisDatosController : Controller
+    {
+        private readonly IntegradorContexto _context;
+
+        public MisDatosController(IntegradorContexto context)
+        {
+            _context = context;
+        }
+
+        // GET: MisDatos/CreatePro
+        public IActionResult CreatePro()
+        {
+            return View();
+        }
+
+        // POST: MisDatos/CreatePro
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreatePro(
+            [Bind("Id,Nombre,Email,Nif,Telefono,Direccion")] Proveedor proveedor)
+        {
+            proveedor.Email = User.Identity.Name;
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(proveedor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        // GET: MisDatos/CreateCli
+        public IActionResult CreateCli()
+        {
+            return View();
+        }
+
+        // POST: MisDatos/CreateCli
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCli(
+            [Bind("Id,Nombre,Email,Nif,Telefono,Direccion")] Cliente cliente)
+        {
+            cliente.Email = User.Identity.Name;
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Escaparate");
+            }
+
+            return View();
+        }
+
+    }
+}
