8b4ead40c9dc0f91369386cd14aab0e035245054
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2023-12-27 22:31:58 +0100

    Comprobación de los datos de los roles

--- Integrador/Controllers/HomeController.cs
+++ Integrador/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Integrador.Data;
 using Integrador.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,14 +8,36 @@ namespace Integrador.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IntegradorContexto _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IntegradorContexto context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            /*string? rolUsuario =  ? "Proveedor" : "Usuario";*/
+            string? emailUsuario = User.Identity.Name;
+
+            if (User.IsInRole("Proveedor"))
+            {
+                var proveedor = _context.Proveedores.FirstOrDefault(p => p.Email == emailUsuario);
+                if (proveedor == null)
+                {
+                    return RedirectToAction("CreatePro", "MisDatos");
+                }
+            }
+            else if (User.IsInRole("Cliente"))
+            {
+                var cliente = _context.Clientes.FirstOrDefault(c => c.Email == emailUsuario);
+                if (cliente == null)
+                {
+                    return RedirectToAction("CreateCli", "MisDatos");
+                }
+            }
+
             return View();
         }
 
