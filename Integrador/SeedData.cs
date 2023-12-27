a9ea59516b9046bc50b0642ab82eb20d679363ec
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2023-12-27 22:33:12 +0100

    Creacion de roles y admin

---
+++ Integrador/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Integrador
+{
+    public class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider services)
+        {
+            // Comprobar y crear los roles predeterminados
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            await CrearRolesAsync(roleManager);
+
+            // Comprobar y crear el administrador predeterminado
+            var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+            await CrearAdminAsync(userManager);
+        }
+
+        private static async Task CrearRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            // Si no existe, se crea el rol predeterminado "Administrador"
+            string nombreRol = "Administrador";
+            var Existe = await roleManager.RoleExistsAsync(nombreRol);
+            if (!Existe)
+                await roleManager.CreateAsync(new IdentityRole(nombreRol));
+
+            // Si no existe, se crea el rol predeterminado "Cliente"
+            nombreRol = "Cliente";
+            Existe = await roleManager.RoleExistsAsync(nombreRol);
+            if (!Existe)
+                await roleManager.CreateAsync(new IdentityRole(nombreRol));
+
+            // Si no existe, se crea el rol predeterminado "Proveedor"
+            nombreRol = "Proveedor";
+            Existe = await roleManager.RoleExistsAsync(nombreRol);
+            if (!Existe)
+                await roleManager.CreateAsync(new IdentityRole(nombreRol));
+        }
+
+        private static async Task CrearAdminAsync(UserManager<IdentityUser> userManager)
+        {
+            // Comprobar si existe el administrador predetermindado
+            var testAdmin = userManager.Users
+            .Where(x => x.UserName == "admin@empresa.com")
+            .SingleOrDefault();
+
+            // Si existe sale de la función
+            if (testAdmin != null) return;
+
+            // Si no existe, se crea el administrador predeterminado "admin@empresa.com"
+            testAdmin = new IdentityUser
+            {
+                UserName = "admin@empresa.com",
+                Email = "admin@empresa.com"
+            };
+
+            string admPasswd = "Admin-123";
+
+            IdentityResult userResult = await userManager.CreateAsync(testAdmin, admPasswd);
+
+            // Se agrega el rol "Administrador" al administrador predeterminado 
+            if (userResult.Succeeded)
+            {
+                await userManager.AddToRoleAsync(testAdmin, "Administrador");
+            }
+        }
+    }
+}
