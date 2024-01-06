5b4407479f012597d8ea777db39d2fdb912ec953
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-01-06 20:51:28 +0100

    Required FKs and Regular expresion

--- Integrador/Models/DetallePedido.cs
+++ Integrador/Models/DetallePedido.cs
@@ -8,10 +8,10 @@ namespace Integrador.Models
         public int Id { get; set; }
 
         [Display(Name = "Número de pedido")]
-        public int PedidoId { get; set; }
+        public required int PedidoId { get; set; }
 
         [Display(Name = "Id del producto")]
-        public int ProductoId { get; set; }
+        public required int ProductoId { get; set; }
 
         [Required(ErrorMessage = "La cantidad es requerida.")]
         public required int Cantidad { get; set; }
@@ -20,6 +20,8 @@ namespace Integrador.Models
 
         [Display(Name = "Precio Unitario")]
         [Range(0.1, double.MaxValue, ErrorMessage = "El precio no puede ser negativo.")]
+        [RegularExpression(@"^[-0123456789]+[0-9.,]*$",
+            ErrorMessage = "El valor introducido debe ser de tipo monetario.")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal? PrecioUnidad // Mayor que 0
         {
