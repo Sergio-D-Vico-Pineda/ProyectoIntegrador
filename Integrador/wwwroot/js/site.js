fa1ffe35dc731fbe97f7dffb6c8d2d1d9f11b52a
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-01-29 20:32:02 +0100

    Modo oscuro DONE

--- Integrador/wwwroot/js/site.js
+++ Integrador/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleDarkMode() {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+}
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
